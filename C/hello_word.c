35b7588073224dc3038f5f3f14afd369e670f51b
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-05-22 15:08:51 -0700

    Practicing from memory

--- C/hello_word.c
+++ C/hello_word.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int main (){
-
-
-  printf("Hello Liz!\n");
+int main(char *argc, char **argv[]){
+  printf("Hello Victory!\n");
+  int var = 15;
+  int *ip;
+  ip = &var;
+  printf("%x\n", ip);
+  printf("%x\n", &var);
+  printf("%d\n", *ip);
 
   return 0;
 }
+
+
