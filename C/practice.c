4b23e49c6c6e2fc1b797ba3a7dc640f66f50ee8b
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-05-22 15:07:46 -0700

    Practice from memory

---
+++ C/practice.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main (){//char *argc, char **argv[]){
+
+
+  printf("Hello Victory!\n");
+  int  var = 15;
+  int  *ip;
+  printf("%x hex\n", &var);
+  printf("%d deci\n", &var);
+  ip = &var;
+  printf("%d deci\n", ip);
+  printf("%x hex\n", *ip );
+
+  int var2 = 2;
+  int *v2;
+  v2 = &var2;
+  printf("%x hex\n", *v2);
+
+  return 0;
+}
