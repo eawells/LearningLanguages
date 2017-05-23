18d269b31298d945f59f9352d8e3e6c143e72d60
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-05-22 17:09:49 -0700

    Making a repl

--- C/practice.c
+++ C/practice.c
@@ -1,22 +1,30 @@
-
 #include <stdio.h>
 
-int main (){//char *argc, char **argv[]){
-
-
-  printf("Hello Victory!\n");
-  int  var = 15;
-  int  *ip;
-  printf("%x hex\n", &var);
-  printf("%d deci\n", &var);
+int main(char *argc, char **argv[]){
+  //printf("Hello Victory!\n");
+  int var = 15;
+  int *ip;
   ip = &var;
-  printf("%d deci\n", ip);
-  printf("%x hex\n", *ip );
+  // printf("%x\n", ip);
+  //  printf("%x\n", &var);
+  //printf("%d\n", *ip);
 
-  int var2 = 2;
-  int *v2;
-  v2 = &var2;
-  printf("%x hex\n", *v2);
+  int square(int x){
+  return x * x;
+  }
+  // printf("%d\n",square(5));
+  // printf("%d\n" ,argc);
+  //  for(int i =0; i< argc; i++){
+  //printf("%s\n", argv[i]);
+  //}
+  int i =0;
+  while (1){
+    printf("repl>");
+    scanf("%d", &i);
+    printf("=>");
+    printf("%d\n",i);
+  }
 
   return 0;
 }
+
