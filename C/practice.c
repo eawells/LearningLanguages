7d5d5ba3a54fd3f4cc67ec144313fe04c14fbb78
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-05-23 14:03:39 -0700

    returns input

--- C/practice.c
+++ C/practice.c
@@ -1,30 +1,35 @@
+
 #include <stdio.h>
 
-int main(char *argc, char **argv[]){
+int main(int *argc, char **argv[]){
   //printf("Hello Victory!\n");
   int var = 15;
   int *ip;
   ip = &var;
-  // printf("%x\n", ip);
-  //  printf("%x\n", &var);
-  //printf("%d\n", *ip);
+   printf("%x\n", ip);
+    printf("%x\n", &var);
+  printf("%d\n", *ip);
 
   int square(int x){
   return x * x;
   }
-  // printf("%d\n",square(5));
-  // printf("%d\n" ,argc);
-  //  for(int i =0; i< argc; i++){
-  //printf("%s\n", argv[i]);
-  //}
+   printf("%d\n",square(5));
+   printf("%d\n" ,argc);
+   for(int i =0; i< argc; i++){
+     printf("%s\n", argv[i]);
+   }
+  //scans input from the command line, and returns that as output
   int i =0;
+  char c;
   while (1){
-    printf("repl>");
-    scanf("%d", &i);
+    printf("New repl>");
+    scanf("%d %c", &i, &c);
+    //scanf("%d", &i);
     printf("=>");
-    printf("%d\n",i);
+    printf("%d %c\n", i, c);
   }
+
+
 
   return 0;
 }
-
