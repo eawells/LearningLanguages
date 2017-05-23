8dfae3588127c8dfa65d8c7084d451df94176328
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-05-23 14:12:07 -0700

    Learning about pointers and functions

--- C/practice.c
+++ C/practice.c
@@ -18,18 +18,6 @@ int main(int *argc, char **argv[]){
    for(int i =0; i< argc; i++){
      printf("%s\n", argv[i]);
    }
-  //scans input from the command line, and returns that as output
-  int i =0;
-  char c;
-  while (1){
-    printf("New repl>");
-    scanf("%d %c", &i, &c);
-    //scanf("%d", &i);
-    printf("=>");
-    printf("%d %c\n", i, c);
-  }
-
-
 
   return 0;
 }
