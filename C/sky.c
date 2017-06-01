ed80a62c1ef0bc2e1c54b81a8a4f26230a9bc05c
Elizabeth <elizabeth.a.wells1@gmail.com>
2017-05-31 17:22:40 -0700

    C files

--- C/sky.c
+++ C/sky.c
@@ -44,10 +44,58 @@ char *eval(char *program){
   return program;
 }
 
-int main(char *argc, char **argv[]){
-  printf("%s\n", eval(read("program")));
-  repl(argc, argv);
+int read_file(){
+  char buff[300];
+  FILE *fp;
+  // fp = fopen("Hello_Liz.sky", "r");
+  //fscanf(fp, "%s", buff);
+
+  // int character =0;
+  /**
+  for(int i =0; i< 10; i++){
+    result = fgets(buff, 30, fp );
+    //character = fgetc(fp);
+    //character = fgets(buff, 30, fp);
+    printf("%s", result);
+    //printf("%c\n", character);
+  }
+  **/
+  fp = fopen("Hello_Liz.sky", "r");
+
+  //fscanf(fp, "%s", buff);
+  // printf("1 : %s\n", buff );
+  char *result;
+   for(int i =0; i< 10; i++){
+    result = fgets(buff, 30, fp );
+    //character = fgetc(fp);
+    printf("%s", result);
+    // printf("%c\n", character);
+     }
+
+  fclose(fp);
+  return 0;
+}
+ 
+int write_file(){
+  FILE *fp;
+  fp = fopen("Hello_Liz.sky", "w");
+
+  fputs("Goodbye Victory\n", fp);
+  fclose(fp);
   
+  return 0;
+}
+  
+
+int  fileio (){
+  return 0;
+}
+
+int main(char *argc, char **argv[]){
+  //printf("%s\n", eval(read("program")));
+  //repl(argc, argv);
+  // read_file();
+  write_file();
   return 0;
   
   }
